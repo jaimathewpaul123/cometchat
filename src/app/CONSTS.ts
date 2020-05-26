6d37c312735e0f7fd9ba0fe3d770386b232e72a1
Jaison James <jaimathewpaul@gmail.com>
2020-05-26 14:45:58 -0400

    new

---
+++ src/app/CONSTS.ts
@@ -0,0 +1,7 @@
+export const COMETCHAT_CONSTANTS = {
+    APP_ID: '19285d316f2bc69',
+    REGION: 'US',
+    API_KEY: '2e251a9fc5df5e7e3bacb847631b3e8f640e22bf'
+}
+
+// http://localhost:4200/?uid=ajaygajra&apiKey=606028d3c3ecff1b2014886376054ada6621ef97&appId=9992e0c46c915d&region=us
